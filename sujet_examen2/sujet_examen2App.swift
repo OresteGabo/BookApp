ad0403c62e824e36ccabe9fc5832287f1026b059
oreste <muhirwa.g.oreste@gmail.com>
2025-11-10 11:43:15 +0100

    Initial Commit

---
+++ sujet_examen2/sujet_examen2App.swift
@@ -0,0 +1,17 @@
+//
+//  sujet_examen2App.swift
+//  sujet_examen2
+//
+//  Created by muhirwa gabo Oreste on 10/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct sujet_examen2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ sujet_examen2Tests/sujet_examen2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  sujet_examen2Tests.swift
+//  sujet_examen2Tests
+//
+//  Created by muhirwa gabo Oreste on 10/11/2025.
+//
+
+import Testing
+@testable import sujet_examen2
+
+struct sujet_examen2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
